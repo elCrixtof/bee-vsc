6e78be26f9d631364c5436060d5b51af7d8be55b
Christian <cristian117acosta@hotmail.com>
2020-08-06 20:11:51 -0500

    Sesion3.3

--- Sesion3/06.py
+++ Sesion3/06.py
@@ -10,3 +10,38 @@ def create_order_detail(self, id_order, id_product, od_amount, od_total):
                return(err)
 
 
+# create table if not exists riesgo_nutricio(
+# 	nua varchar(06) not null,
+#     periodo varchar(30) not null,
+#     padecimiento_actual varchar(50),
+#     cirugias varchar(50),
+#     tratamiento varchar(50),
+#     n_comidas int not null,
+#     desayuno boolean not null,
+#     comida boolean not null,
+#     cena boolean not null,
+#     colaciones boolean not null,
+#     hora_apetito varchar(20) not null,
+#     lugar_comida varchar(30) not null,
+#     cocinero varchar(30) not null,
+#     servicios_basicos boolean not null,
+#     cantidad_agua float(6,2) not null,
+#     consumo_tablaco boolean not null,
+#     cantidad_tabaco float(6,2) not null,
+#     consumo_alcohol boolean not null,
+#     cantidad_alcohol float(6,2) not null,
+#     consumo_azucar boolean not null,
+#     cantidad_azucar float(6,2) not null,
+#     criterio_1 varchar(200) not null,
+# 	criterio_2 varchar(200) not null,
+# 	criterio_3 varchar(200) not null,
+# 	criterio_4 varchar(200) not null,
+# 	criterio_5 varchar(200) not null,
+#     diagnostico varchar(200) not null,
+#     institucion varchar(60) not null,
+#     primary key (nua, periodo),
+#     constraint fk_riesgo_alumno foreign key(nua)
+# 		references alumno(nua)
+#         on delete cascade
+#         on update cascade
+# )engine = innodb;
